fc4a530e94244d67b55ecea90708262ac4177528
pekrogh <pe@kroghconsulting.dk>
2015-11-18 20:17:21 +0100

    Initial commit

---
+++ src/main/java/com/example/GradledemoApplication.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@EnableAutoConfiguration
+public class GradledemoApplication {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradledemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/Greeting.java
@@ -0,0 +1,23 @@
+package com.example;
+
+/**
+ * Created by pek on 07/10/2015.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
